1aa3aa41a098ab0eba9b701631b3dab5125e2d5a
Sagarrpandey <52660466+SagarrPandey@users.noreply.github.com>
2020-03-13 20:31:39 +0530

    changes for live site

--- src/App.js
+++ src/App.js
@@ -10,16 +10,17 @@ import {
 import Home from './pages/homePage';
 import QandA from './pages/QandAPage';
 import Answer from './pages/answerPage';
-
+import User from './pages/userPage';
 
 class App extends Component {
   render(){
     return (
       <Router>
         <Switch>
-        <Route exact path="/" component="Home"><Home/></Route>
+        <Route exact path="/QandAjs" component="Home"><Home/></Route>
         <Route exact path="/QandA" component="QandA"><QandA/></Route>
         <Route exact path="/Answer" component="Answer"><Answer/></Route>
+        <Route exact path="/user" component="User"><Answer/></Route>
         </Switch>
       </Router>
     );
