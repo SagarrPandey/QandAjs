e85044cfe5c3cfd5ceea4f7492d08374373a2061
Sagarrpandey <52660466+SagarrPandey@users.noreply.github.com>
2020-03-13 10:32:31 +0530

    Updates

---
+++ precache-manifest.d5fe86113a6c5b2a697c82e6244c7250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa8ab681dae209a4f2fd8dac7b194e8",
+    "url": "/QandAjs/index.html"
+  },
+  {
+    "revision": "29e2147d341a47941a2f",
+    "url": "/QandAjs/static/css/main.1164a892.chunk.css"
+  },
+  {
+    "revision": "04eaa1d6551335c7ca93",
+    "url": "/QandAjs/static/js/2.307c358f.chunk.js"
+  },
+  {
+    "revision": "adbe8770dfe80185b1d2467350068428",
+    "url": "/QandAjs/static/js/2.307c358f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29e2147d341a47941a2f",
+    "url": "/QandAjs/static/js/main.520eca56.chunk.js"
+  },
+  {
+    "revision": "b61972e81928be8e8088",
+    "url": "/QandAjs/static/js/runtime-main.14508ebc.js"
+  },
+  {
+    "revision": "f34d557bdf1dc97183cd4b999ee7321d",
+    "url": "/QandAjs/static/media/banner-1.f34d557b.jpg"
+  }
+]);
