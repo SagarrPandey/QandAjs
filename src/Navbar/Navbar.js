d761a43a15dabec6d9d31edfef2dc3edacbc240e
Sagarrpandey <52660466+SagarrPandey@users.noreply.github.com>
2020-03-16 01:21:25 +0530

    navbar converted into react-bootstrap

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,47 +1,44 @@
 import React from 'react';
+import {Navbar,Nav,Container,Form,FormControl,Button} from 'react-bootstrap';
+
 import '../Static/css/qanda/style.css';
 import '../Static/css/bootstrap.css';
 import '../Static/css/style.css';
+import '../Static/css/navbar/navbar.css'
 import { FaLaptopCode } from 'react-icons/fa';
 
 const navbar = () =>{
     return(
     
-        <nav className="navbar navbar-expand-md bg-yellow navbar-light fixed-top py-1" id="main-nav">
-        <div className="container">
-          <a href="/" className="navbar-brand pr-5">
-                    <FaLaptopCode size='2em'/>
+        <Navbar bg="yellow" expand="md" className="py-1" id="main-nav">
+        <Container >
+          <Navbar.Brand href="/" className="pr-5">
+                    <FaLaptopCode size='1.5em'/>
              <span className="ml-2 font-custom">Q & A</span>
-            </a>
-          <div className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-            <div className="navbar-toggler-icon"></div>
-          </div>
-          <div className="collapse navbar-collapse" id="navbarCollapse">
-                <form className="form-inline">
-                    <input className="form-control form-control-sm mr-sm-2 ml-sm-5" type="search" placeholder="Search" aria-label="Search"/>
+            </Navbar.Brand>
+          <Navbar.Toggle area-control="navbarCollapse"/>
+            
+          <Navbar.Collapse id="navbarCollapse">
+                <Form inline>
+                    <Form.Control className="form-control-sm mr-sm-2 ml-sm-5" type="text" placeholder="Search" aria-label="Search"/>
 
-                </form>
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a href="/" className="nav-link">Home</a>
-              </li>
-              <li className="nav-item">
-                <a href="QandA" className="nav-link">QandA</a>
-              </li>
-              <li className="nav-item">
-                <a  className="nav-link text-primary"  data-toggle="modal" data-target="#login">login</a>
-              </li>
-              <li className="nav-item">
-                <a  className="nav-link btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#signup">Sign up</a>
-              </li>
+                </Form>
+            <Nav className="ml-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="QandA">QandA</Nav.Link>
+              <Nav.Link data-toggle="modal" data-target="#login" className="text-primary">login</Nav.Link>
+              <Nav.Link  data-toggle="modal" data-target="#signup">
+                        <Button size="sm" variant="outline-primary">Sign up</Button>
+              </Nav.Link>
+              
 
 
 
-            </ul>
-          </div>
+            </Nav>
+          </Navbar.Collapse>
 
-        </div>
-      </nav>
+        </Container>
+      </Navbar>
 
 
 
